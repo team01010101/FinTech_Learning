7de18f7ab19715e42cc73a86db4b7495303d2d39
Maria Elena Lorenzo Suarez <mariaelena.lorenzo@acsiongroup.com>
2018-08-26 06:48:22 -0300

    Hop Updates

--- FinTechWebApp/FinTechWebApp/Controllers/UserHomeController.cs
+++ FinTechWebApp/FinTechWebApp/Controllers/UserHomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using FinTechWebApp.Data;
 using FinTechWebApp.Helpers;
+using FinTechWebApp.MicroServices;
 using FinTechWebApp.Models.Hackathon;
 using FinTechWebApp.Models.Services;
 
@@ -61,12 +63,26 @@ namespace FinTechWebApp.Controllers
                 {
                     using (var context = new HackathonContext())
                     {
-                        var loan = context.Loans.Find(Constants.LoanGuid());
+                        var loan = context.Loans.FirstOrDefault();
                         if (loan != null)
                         {
-                            context.Loans.Remove(loan);
-                            context.SaveChanges();
-                            return RedirectToAction("ShowLoan");
+                            var payment =
+                                context.Payments.FirstOrDefault(x => x.Status == (short)Enumerators.PaymentStatus.NotPayed);
+                            HOP_PaymentClient hopClient = new HOP_PaymentClient();
+                            string redirectUrl = "http://localhost:50162" + Url.Action("Index", new {username = "elena"});
+                            //var url = hopClient.PaymentRequest(loan.User.EmailAddress, payment.InvoicedAmount, "",
+                            //    payment.InvoiceNumber,
+                            //    redirectUrl);
+
+                            var a = hopClient.GetAuthorizationToken(payment.InvoiceNumber, payment.InvoicedAmount);
+
+                            var url = "https://testhop.hakrinbank.com/gateway/service/PaymentController.php?TokenID=" + a +
+                                   "&Email=" + "manson.mels@gmail.com" + "&Amount=" + payment.InvoicedAmount + "&Desc=" + "Description" + "&Inv=" + payment.InvoiceNumber +
+                                   "&returnURL=" + redirectUrl;
+                            Response.Redirect(url);
+                            //context.Loans.Remove(loan);
+                            //context.SaveChanges();
+                            //return RedirectToAction("ShowLoan");
                         }
                     }
                 }
